8f1ab6f9ee69c7e97345c3bae6a7610e2fcc07bb
Wren <renprofessional97@gmail.com>
2022-06-09 11:29:58 +0100

    Saving progress

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>My Calculator</h1>
-      <h2>{input}</h2>
+      <h2 data-testid="display">{input}</h2>
       <Operators handleClick={handleClick} />
       <Numbers handleClick={handleClick} />
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,44 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+describe("Header tests", () => {
+  //  Makes sure that the application has this specific text as the heading
+  it("should find the test of the header", () => {
+    // What file we are testing
+    render(<App />);
+    // Query methods that we are using
+    const headerElement = screen.getByText(/My Calculator/i);
+    // Assertions
+    expect(headerElement).toBeInTheDocument();
+  });
+
+  it("should find the element of the header", () => {
+    render(<App />);
+    const headingElement = screen.getByRole("heading", {
+      name: "My Calculator",
+    });
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  it("should have a h1 tag as the main header element", () => {
+    render(<App />);
+    const headingElementType = screen.getByText(/My Calculator/i);
+    expect(headingElementType).toContainHTML("h1");
+  });
+});
+
+it("should display 4 when the user enters 2+2", () => {
   render(<App />);
-  const linkElement = screen.getByText(/My Calculator/i);
-  expect(linkElement).toBeInTheDocument();
+  const twoButton = screen.getByText("2");
+  const plusButton = screen.getByText("+");
+  const equalsButton = screen.getByText("=");
+
+  fireEvent.click(twoButton);
+  fireEvent.click(plusButton);
+  fireEvent.click(twoButton);
+  fireEvent.click(equalsButton);
+
+  const display = screen.getByTestId("display");
+  expect( display ).toHaveTextContent( "4" );
+  
 });
